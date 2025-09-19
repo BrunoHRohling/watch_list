fcebae914f9b13ab54c9c431be06e7d88afa0a69
Bruno <brunorohling340@gmail.com>
2025-09-19 19:18:14 -0300

    some of stylization in index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,19 +3,20 @@
 @section('title', 'Watch List')
 
 @section('content')
-    <nav class="flex flex-row-reverse">
-        <a href="{{ route('items.create') }}" class="button text-amber-800">Add Item</a>
+    <nav class="flex flex-row">
+        <a href="{{ route('items.create') }}" class="button border-blue-900 text-blue-900">Add Item</a>
     </nav>
 
-
-        <form action="{{ route('items.index') }}" method="GET">
-            <div>
-                <label for="q" class="font-medium">Search by title:</label>
-                <input type="text" id="q" name="q" value="{{ request('q') }}" placeholder="Type a title..." class="">
+    <form action="{{ route('items.index') }}" method="GET">
+        <div class="search-filter">
+            <div class="p-1 grid">
+                <label for="q" class="font-medium">Title:</label>
+                <input type="text" id="q" name="q" value="{{ request('q') }}" placeholder="Type a title..."
+                    class="border-1 rounded-md p-1">
             </div>
-            <div>
-                <label for="s">Filter by status:</label>
-                <select name="s" id="s">
+            <div class="p-1 grid">
+                <label for="s" class="font-medium">Status:</label>
+                <select name="s" id="s" class="border-1 rounded-md p-1">
                     <option value="">---</option>
                     <option value="planned">Planned</option>
                     <option value="watching">Watching</option>
@@ -23,94 +24,102 @@
                     <option value="dropped">Dropped</option>
                 </select>
             </div>
-            <div>
-                <label for="t">Filter by type:</label>
-                <select name="t" id="t">
+            <div class="p-1 grid">
+                <label for="t" class="font-medium">Type:</label>
+                <select name="t" id="t" class="border-1 rounded-md p-1">
                     <option value="">---</option>
                     <option value="movie">Movie</option>
                     <option value="series">Series</option>
                 </select>
             </div>
-            <div>
-                <button type="submit">Search</button>
+            <div class="p-1 grid">
+                <button type="submit" class="bg-blue-600 rounded-lg text-white font-medium p-2 h-10 mt-5">Search</button>
             </div>
             @if (request(['q', 's', 't']))
-                <div>
-                    <a href="{{ route('items.index') }}">Clear</a>
+                <div class="p-1 grid">
+                    <a href="{{ route('items.index') }}" class="button text-blue-800 h-10 mt-5">Clear</a>
                 </div>
             @endif
-        </form>
-
-    @forelse ($items as $item)
-        <div>
-            @isset($item->poster_url)
-                <img src="{{ $item->poster_url }}" alt="Item image">
-            @else
-                <img src="{{ asset('storage/images/Image-not-found.png') }}" alt="Item image">
-            @endisset
-
-            <div>
-                <a href="{{ route('items.show', ['item' => $item]) }}">
-                    {{ $item->title }}
-                    @isset($item->year)
-                        ({{ $item->year }})
-                    @endisset
-                </a>
-            </div>
-
-            <div>
-                <form action="{{ route('items.rating', ['item' => $item]) }}" method="POST">
-                    @csrf
-                    @method('PATCH')
-                    <div>
-                        <label for="rating"></label>
-                        <input type="number" id="rating_{{ $item->id }}" name="rating" value="{{ $item->rating }}">
-                        @error('rating')
-                            <p>{{ $message }}</p>
-                        @enderror
-
-                        <button type="submit">Update rating</button>
-                    </div>
-                </form>
-            </div>
-
-            <div>
-                <form action="{{ route('items.status', ['item' => $item]) }}" method="POST">
-                    @csrf
-                    @method('PATCH')
-                    <div>
-                        <label for="status"></label>
-                        <select name="status" id="status_{{ $item->id }}">
-                            <option value="planned" {{ $item->status == 'planned' ? 'selected' : '' }}>Planned</option>
-                            <option value="watching" {{ $item->status == 'watching' ? 'selected' : '' }}>Watching</option>
-                            <option value="watched" {{ $item->status == 'watched' ? 'selected' : '' }}>Watched</option>
-                            <option value="dropped" {{ $item->status == 'dropped' ? 'selected' : '' }}>Dropped</option>
-                        </select>
-                        @error('status')
-                            <p>{{ $message }}</p>
-                        @enderror
-
-                        <button type="submit">Update status</button>
-                    </div>
-                </form>
-            </div>
-
-            <div>
-                <a href="{{ route('items.edit', ['item' => $item]) }}">Edit</a>
-            </div>
-
-            <div>
-                <form action="{{ route('items.destroy', ['item' => $item]) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
-            </div>
-
         </div>
-    @empty
-        <div>There is no watch list!</div>
-    @endforelse
+    </form>
+
+    <div class="grid grid-cols-4">
+        @forelse ($items as $item)
+            <div class="col-auto m-2">
+                <div class="p-2 my-2 text-center">
+                    <a href="{{ route('items.show', ['item' => $item]) }}" class="font-medium text-blue-700">
+                        {{ $item->title }}
+
+                        @isset($item->year)
+                            ({{ $item->year }})
+                        @endisset
+
+                        <div class="grid justify-center">
+                            @isset($item->poster_url)
+                                <img src="{{ $item->poster_url }}" alt="Item image" class="rounded-4xl" height="100%">
+                            @else
+                                <img src="{{ asset('storage/images/Image-not-found.png') }}" alt="Item image" class="rounded-4xl" height="100%">
+                            @endisset
+                        </div>
+                    </a>
+                </div>
+
+                <div class="my-2">
+                    <form action="{{ route('items.rating', ['item' => $item]) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <div>
+                            <label for="rating" class="">Rating: </label>
+                            <input type="number" id="rating_{{ $item->id }}" name="rating"
+                                value="{{ $item->rating }}">
+                            @error('rating')
+                                <p>{{ $message }}</p>
+                            @enderror
+
+                            <button type="submit">Update rating</button>
+                        </div>
+                    </form>
+                </div>
+
+                <div>
+                    <form action="{{ route('items.status', ['item' => $item]) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <div>
+                            <label for="status"></label>
+                            <select name="status" id="status_{{ $item->id }}">
+                                <option value="planned" {{ $item->status == 'planned' ? 'selected' : '' }}>Planned</option>
+                                <option value="watching" {{ $item->status == 'watching' ? 'selected' : '' }}>Watching
+                                </option>
+                                <option value="watched" {{ $item->status == 'watched' ? 'selected' : '' }}>Watched</option>
+                                <option value="dropped" {{ $item->status == 'dropped' ? 'selected' : '' }}>Dropped</option>
+                            </select>
+                            @error('status')
+                                <p>{{ $message }}</p>
+                            @enderror
+
+                            <button type="submit">Update status</button>
+                        </div>
+                    </form>
+                </div>
+
+                <div>
+                    <a href="{{ route('items.edit', ['item' => $item]) }}">Edit</a>
+                </div>
+
+                <div>
+                    <form action="{{ route('items.destroy', ['item' => $item]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </div>
+
+            </div>
+        @empty
+            <div>There is no watch list!</div>
+        @endforelse
+    </div>
 
     @if ($items->count())
         <nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,24 +10,28 @@
 
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
+        .container {
+            @apply mx-40 my-4
+        }
+
         .link {
-            @apply decoration-amber-600 text-amber-800 underline
+            @apply decoration-blue-600 bg-blue-900 underline
         }
 
         .button {
-            @apply border-2 p-1 rounded-xl font-bold 
+            @apply border-2 p-2 rounded-lg font-bold
         }
 
         .navbar {
-            @apply bg-amber-800 h-16
+            @apply bg-blue-900 h-16
         }
 
         .page-title {
             @apply text-white p-4 font-bold text-xl
         }
 
-        .content {
-            @apply p-4
+        .search-filter {
+            @apply flex flex-row
         }
     </style>
     {{-- blade-formatter-enable --}}
@@ -42,12 +46,12 @@
         </div>
     </div>
 
+
         <div class="flex justify-center">
-            <div class="content">
+            <div class="container">
                 @yield('content')
             </div>
         </div>
-
 </body>
 
 </html>
